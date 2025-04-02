6aa1a8900fc004d92ebcc57bb6bdd1e0ddd01123
AeonFr <frankno.94@gmail.com>
2025-04-02 14:35:32 +0200

    Remove test mocks that were not needed

--- src/models/BaseModel.test.ts
+++ src/models/BaseModel.test.ts
@@ -35,11 +35,6 @@ describe('BaseModel', () => {
 
   describe('findById', () => {
     it('returns the item with matching id', () => {
-      // Mock the findById implementation for testing
-      BaseModel.findById = function<T>(this: any, id: string): T | null {
-        return this.getAll().find((item: any) => item.id === id) || null;
-      };
-
       expect(BaseModelWithTable.findById('1')).toEqual(tableData[0]);
       expect(BaseModelWithTable.findById('2')).toEqual(tableData[1]);
       expect(BaseModelWithTable.findById('3')).toBeNull();
@@ -48,11 +43,6 @@ describe('BaseModel', () => {
 
   describe('findBy', () => {
     it('returns items with matching field value', () => {
-      // Mock the findBy implementation for testing
-      BaseModel.findBy = function<T>(this: any, field: string, value: any): T[] {
-        return this.getAll().filter((item: any) => item[field] === value);
-      };
-
       expect(BaseModelWithTable.findBy('name', 'foo')).toEqual([tableData[0]]);
       expect(BaseModelWithTable.findBy('name', 'bar')).toEqual([tableData[1]]);
       expect(BaseModelWithTable.findBy('name', 'baz')).toEqual([]);
