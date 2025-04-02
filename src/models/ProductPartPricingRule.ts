76ecf5bf1021e3cf69507324eb176e42d6e2e715
AeonFr <frankno.94@gmail.com>
2025-04-02 15:51:03 +0200

    Rename product_part reference columns from the product_part_pricing_rules table

--- src/models/ProductPartPricingRule.ts
+++ src/models/ProductPartPricingRule.ts
@@ -1,19 +1,18 @@
 import BaseModel from "./BaseModel";
-import ProductPart from "./ProductPart";
 
 export interface ProductPartPricingRuleDB {
   id: string;
   description: string;
-  product_part_id_base: string;
-  product_part_id_dep: string;
+  product_part_id_1: string;
+  product_part_id_2: string;
   price_adjustment: number;
 }
 
 export default class ProductPartPricingRule extends BaseModel {
   readonly id: string;
   readonly description: string;
-  readonly product_part_id_base: string;
-  readonly product_part_id_dep: string;
+  readonly product_part_id_1: string;
+  readonly product_part_id_2: string;
   readonly price_adjustment: number;
 
   static readonly table = "product_part_pricing_rules";
@@ -22,8 +21,8 @@ export default class ProductPartPricingRule extends BaseModel {
     super();
     this.id = pricingRuleDB.id;
     this.description = pricingRuleDB.description;
-    this.product_part_id_base = pricingRuleDB.product_part_id_base;
-    this.product_part_id_dep = pricingRuleDB.product_part_id_dep;
+    this.product_part_id_1 = pricingRuleDB.product_part_id_1;
+    this.product_part_id_2 = pricingRuleDB.product_part_id_2;
     this.price_adjustment = pricingRuleDB.price_adjustment;
   }
 }
